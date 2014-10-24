f6e70733052922b780246932fa4248a50a3d421a
TheoWinter <theo.winter@gmail.com>
2014-10-24 10:15:49 +0200

    added ApplicationSettings class, added additional fields to Shortcut DataObject class

---
+++ Shortcutter/Shortcutter/DataObjects/ApplicationSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Shortcutter
+{
+	public class ApplicationSettings
+	{
+		public Boolean NotificationsEnabled{ get; set;}
+		public int WaittimeAfterContextSwitch{ get; set;}
+		public int WaittimeBeforeNextNotification{ get; set;}
+
+		public ApplicationSettings ()
+		{
+		}
+	}
+}
+
--- Shortcutter/Shortcutter/DataObjects/Shortcut.cs
+++ Shortcutter/Shortcutter/DataObjects/Shortcut.cs
@@ -7,6 +7,8 @@ namespace Shortcutter
 		public string ApplicationName{ get; set;}
 		public string Description{ get; set; }
 		public string ShortcutAction{ get; set; }
+		public Boolean learnedShortcut{ get; set;}
+		private int nofShowed;
 
 		public Shortcut ()
 		{
@@ -17,7 +19,18 @@ namespace Shortcutter
 			this.ApplicationName = ApplicationName;
 			this.Description = Description;
 			this.ShortcutAction = Shortcut;
+			this.nofShowed = 0;
+			this.learnedShortcut = false;
+		}
+
+		public void IncrementNofShowed()
+		{
+			++nofShowed;
+		}
+
+		public void ResetNofShowed()
+		{
+			nofShowed = 0;
 		}
 	}
 }
-
